10fc8ac5eb6c2b11e7e5716c1880a3eaa757b9ab
Pedro Pastuk <pedro.pastuk@alexandria.solar>
2022-02-01 08:22:42 -0300

    [#7] error message fix

--- src/shared/errors/message-errors.ts
+++ src/shared/errors/message-errors.ts
@@ -4,7 +4,7 @@ export class MessagesError {
 
     public INVALID_LENGTH: string = 'Digite uma Linha Digitável que contenha 47 ou 48 caracteres';
 
-    public INVALID_FIRST_CHAR: string = 'Linhas Digitáveis de Convênio sempre começam com o número "8"';
+    public INVALID_FIRST_CHAR: string = 'Linhas Digitáveis de Convênio sempre começam com o número 8';
     
     public INVALID_DV: string = 'Dígitos Verificadores não correspondem com o cálculo';
     
